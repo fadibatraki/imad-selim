9102a2f27321cb69ddd3ffdc25345b59af681a3e
fadi123321 <fadibatraki321@gmail.com>
2026-08-20 17:15:07 +0300

    edit reel

--- src/data/instagram-reels.ts
+++ src/data/instagram-reels.ts
@@ -7,18 +7,23 @@ export interface InstagramReel {
 /** أضف روابط Reels هنا. معاملات المشاركة في نهاية الرابط ليست مطلوبة. */
 export const instagramReels: InstagramReel[] = [
   {
-    id: 1,
+    id: 4,
     title: "Imad Selim - Reel 01",
+    url: "https://www.instagram.com/reel/DcOs7DWIQ90/",
+  },
+  {
+    id: 1,
+    title: "Imad Selim - Reel 02",
     url: "https://www.instagram.com/reel/DYzFtQco54S/",
   },
   {
     id: 2,
-    title: "Imad Selim - Reel 02",
+    title: "Imad Selim - Reel 03",
     url: "https://www.instagram.com/reel/DaiF9F0sjEV/",
   },
   {
     id: 3,
-    title: "Imad Selim - Reel 03",
+    title: "Imad Selim - Reel 04",
     url: "https://www.instagram.com/reel/DaQR3zAIWzz/",
   },
 ];
